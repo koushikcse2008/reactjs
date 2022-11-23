67505aca6c0697aab4a04c7f5e876c20e1438eab
Koushik Kumar Rudra <koushikcse2008@gmail.com>
2022-11-23 08:46:54 +0530

    Second commit

--- react-style/src/component/InlineStyle.jsx
+++ react-style/src/component/InlineStyle.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import logo from './logo192.png';
+
 
 const InlineStyle = () => {
   return (
     <>
-    <h2>Inline Color</h2>
-    <p style={{"color": "green"}}>Green color</p>
+    <h2>Inline Style Options</h2>
+    <p style={{"color": "green"}}>Font color</p>
+    <p style={{"backgroundColor": "blue"}}>Background color</p>
+    <p style={{"fontSize": "50px"}}>Font Size</p>
+    <p style={{"fontStyle": "italic"}}>Font Style Italic</p>
+    <p><img src={logo} alt="" /></p>
     <hr />
     </>
   )
